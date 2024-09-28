1c408470e8f27f79d5f75dde14980834a2b4f141
McJay L. Senin <mcjaysenin09@gmail.com>
2024-09-28 21:32:28 +0800

    Create navbardetect.js

---
+++ js/navbardetect.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+    const sections = document.querySelectorAll(".section");
+
+    window.addEventListener("scroll", () => {
+        let currentSection = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                currentSection = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.classList.remove("active");
+            if (link.getAttribute("href") === `#${currentSection}`) {
+                link.classList.add("active");
+            }
+        });
+    });
+});
